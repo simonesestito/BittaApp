defa05b855293e7e96cdca897b6d00ba1e90fef0
Simone Sestito <simone@simonesestito.com>
2022-12-26 09:45:12 +0100

    Add Android 12 splash screen

--- BittaApp/app/src/main/java/com/bitta/app/MainActivity.kt
+++ BittaApp/app/src/main/java/com/bitta/app/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.bitta.app.ui.routes.AppNavigator
@@ -15,6 +16,7 @@ import com.bitta.app.ui.theme.BittaAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
