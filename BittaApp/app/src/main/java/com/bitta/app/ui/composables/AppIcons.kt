02289db23ab8291e4641073e4943dc83d92eb8f3
Simone Sestito <simone@simonesestito.com>
2022-12-26 12:15:30 +0100

    AppIcons.kt: List only used icons for better search

--- BittaApp/app/src/main/java/com/bitta/app/ui/composables/AppIcons.kt
+++ BittaApp/app/src/main/java/com/bitta/app/ui/composables/AppIcons.kt
@@ -1,5 +1,38 @@
 package com.bitta.app.ui.composables
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.*
 
-val AppIcons = Icons.Outlined
+/**
+ * List only used icons
+ */
+object AppIcons {
+    val Warning = Icons.App.Warning
+    val Error = Icons.App.Error
+    val Handyman = Icons.App.Handyman
+    val ProductionQuantityLimits = Icons.App.ProductionQuantityLimits
+    val Toll = Icons.App.Toll
+    val Dangerous = Icons.App.Dangerous
+    val MoreHoriz = Icons.App.MoreHoriz
+    val ArrowBack = Icons.App.ArrowBack
+    val Close = Icons.App.Close
+    val Flag = Icons.App.Flag
+    val RunningWithErrors = Icons.App.RunningWithErrors
+    val List = Icons.App.List
+    val Check = Icons.App.Check
+    val Search = Icons.App.Search
+    val Info = Icons.App.Info
+    val Payments = Icons.App.Payments
+    val ArrowRight = Icons.App.ArrowRight
+    val Description = Icons.App.Description
+    val ReceiptLong = Icons.App.ReceiptLong
+    val TipsAndUpdates = Icons.App.TipsAndUpdates
+    val Send = Icons.App.Send
+    val EditNote = Icons.App.EditNote
+}
+
+/**
+ * Be consistent with the app icons style
+ */
+val Icons.App
+    get() = Icons.Outlined
